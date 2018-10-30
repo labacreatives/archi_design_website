7086a5da0511670603dc0d7d2a351b0f425d6c62
adonias <adonimm@gmail.com>
2018-10-30 11:34:01 +0300

    Done with simple mail sending with phpmailer and mailtrap

---
+++ Email/ContactForm.php
@@ -0,0 +1,38 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+//Preparing the mail
+$mail = new PHPMailer(true);                    // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.mailtrap.io';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = '6aae844fe34624';                 // SMTP username
+    $mail->Password = '2f386047957773';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('contact@labacreatives.com', 'adonias');
+    $mail->addAddress('info@labacreatives.com', 'info');     // Add a recipient
+    $mail->addReplyTo('info@example.com', 'Information');
+
+    //Content
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the  message body';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
